fff27a409562230615f23ba9d47763cf313e42ab
Ashfaq Ahmed <ahmed.ashfaq9281@gmail.com>
2021-11-01 21:40:36 +0600

    Vue slot implemented

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,16 @@
     <button class="bg-gray-600 mt-8 px-6 py-3 rounded-xl text-white uppercase" @click="toggleModal">Show Modal</button>
   </div>
   <div v-if="showModal">
-    <modal :title="title" :content="content" @close="toggleModal" theme="dark" />
+    <modal @close="toggleModal" theme="dark">
+      <h1 class="text-4xl">Modal Title</h1>
+      <p class="text-xl">Modal Conent</p>
+      <template v-slot:links>
+        <div class="mt-6">
+          <a class="border border-yellow-400 px-6 py-3 rounded-lg mr-5" href="#">Sign up</a>
+          <a class="border border-green-400 px-6 py-3 rounded-lg" href="#">More Info</a>
+        </div>
+      </template>
+    </modal>
   </div>
 </template>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="fixed top-0 bg-gray-900 bg-opacity-95 flex h-screen w-full" @click.self="closeModal">
-    <div :class="{dark: theme === 'dark'}" class="bg-gray-200 m-auto text-black w-96 h-96 rounded-lg flex flex-col justify-center items-center">
-      <h1 class="text-4xl">{{ title }}</h1>
-      <p class="text-xl">{{ content }}</p>
+    <div :class="{ dark: theme === 'dark' }" class="bg-gray-200 m-auto text-black w-96 h-96 rounded-lg flex flex-col justify-center items-center">
+      <slot></slot>
+      <div>
+        <slot name="links">
+          <div class="mt-6">
+            <!-- Default name slot -->
+            <a class="border border-red-600 px-6 py-3 rounded-lg" href="#">Default</a>
+          </div>
+        </slot>
+      </div>
     </div>
   </div>
 </template>
@@ -10,7 +17,7 @@
 <script>
 export default {
   name: 'Modal',
-  props: ['title', 'content', 'theme'],
+  props: ['theme'],
   methods: {
     closeModal() {
       this.$emit('close')
