bb71ec3b0a818e81e0fd51b25bf3c19359ae8784
Ashfaq Ahmed <ahmed.ashfaq9281@gmail.com>
2021-11-01 21:10:20 +0600

    Modal functionality completed

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
-
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="flex flex-col mt-16 items-center">
+    <h1 class="text-4xl px-8 py-16 mx-auto bg-green-400 rounded-xl text-center">{{ header }}</h1>
+    <button class="bg-gray-600 mt-8 px-6 py-3 rounded-xl text-white uppercase" @click="toggleModal">Show Modal</button>
+  </div>
+  <div v-if="showModal">
+    <modal :title="title" :content="content" @close="toggleModal" theme="dark" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Modal,
+  },
+  data() {
+    return {
+      header: 'Demonstration of Modal by Vue.js',
+      showModal: false,
+      title: 'Modal Title',
+      content: 'Modal content',
+    }
+  },
+
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
   },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fixed top-0 bg-gray-900 bg-opacity-95 flex h-screen w-full" @click.self="closeModal">
+    <div :class="{dark: theme === 'dark'}" class="bg-gray-200 m-auto text-black w-96 h-96 rounded-lg flex flex-col justify-center items-center">
+      <h1 class="text-4xl">{{ title }}</h1>
+      <p class="text-xl">{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: ['title', 'content', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
